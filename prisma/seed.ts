4e0fd5c17e05c231e2831f296b9c81ac86b20b40
伊地知 侑一 <ki00215@kyowa.com>
2026-05-01 11:32:03 +0900

    seedを実行した

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const prisma = new PrismaClient();
 async function main() {
@@ -18,3 +20,75 @@ async function main() {
 				description: "こんにちは。よろしくお願いします🐕",
 			};
 		});
+
+	const createPosts = () => {
+		return [...Array(10)].map(() => {
+			return {
+				image: images[Math.floor(Math.random() * images.length)],
+				caption: "かわいいワンちゃん🐶",
+			};
+		});
+	};
+
+	const createComments = (userId: string, postId: string) => {
+		const comments = [
+			"かわいい😍",
+			"可愛い❤️",
+			"すてき👏",
+			"素敵✨",
+			"ナイス🙌",
+			"いいね👍",
+			"👏👏👏",
+			"🎉🎉🎉",
+			"🍾🍾🍾",
+			"🙌🙌🙌",
+		];
+		return {
+			text: comments[Math.floor(Math.random() * comments.length)],
+			userId,
+			postId,
+		};
+	};
+
+	for (const user of users) {
+		await prisma.user.upsert({
+			where: { email: user.email },
+			update: {},
+			create: {
+				...user,
+				posts: {
+					create: createPosts(),
+				},
+			},
+		});
+	}
+
+	const createdUsers = await prisma.user.findMany();
+	const createdPosts = await prisma.post.findMany();
+	const comments = createdPosts.flatMap((post) => {
+		return createdUsers
+			.map((user) => {
+				return {
+					postId: post.id,
+					userId: user.id,
+				};
+			})
+			.map((ids) => {
+				return createComments(ids.userId, ids.postId);
+			});
+	});
+
+	await prisma.comment.createMany({
+		data: comments,
+	});
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
